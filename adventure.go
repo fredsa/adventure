c91373c2f212f6ff81417cde624aa839db04b9fe
Fred Sauer <fredsa@google.com>
2024-03-09 11:28:41 -0800

    invalid memory address or nil pointer dereference

---
+++ adventure.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"github.com/googleapis/gax-go/v2/apierror"
+	"google.golang.org/api/option"
+	"google.golang.org/grpc/status"
+)
+
+const initialPromptFile = "prompt.md"
+
+// getBytes returns the file contents as bytes.
+func getBytes(path string) []byte {
+	bytes, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Error reading file bytes %v: %v\n", path, err)
+	}
+	return bytes
+}
+
+func main() {
+	ctx := context.Background()
+
+	// New client, using API key authorization.
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+	if err != nil {
+		log.Fatalf("Error creating client: %v\n", err)
+	}
+	defer client.Close()
+	// log.Printf("client: %v", client)
+
+	// Configure desired model.
+	model := client.GenerativeModel("gemini-pro")
+	// log.Printf("model: %v", model)
+
+	// Initialize new chat session.
+	session := model.StartChat()
+	// log.Printf("session: %v", session)
+
+	// // Establish chat history.
+	// session.History = []*genai.Content{{
+	// 	Role:  "user",
+	// 	Parts: []genai.Part{genai.Text(getBytes(initialPromptFile))},
+	// }}
+
+	send(ctx, session, string(getBytes(initialPromptFile)))
+	chat(ctx, session)
+}
+
+func chat(ctx context.Context, session *genai.ChatSession) {
+	for {
+		var action string
+		fmt.Print("\n>> ")
+		fmt.Scanln(&action)
+
+		fmt.Println("---------------------------------------------")
+		send(ctx, session, action)
+	}
+}
+
+func send(ctx context.Context, session *genai.ChatSession, text string) {
+	resp, err := session.SendMessage(ctx, genai.Text(text))
+	if err != nil {
+		log.Printf("Error sending message: err=%v\n", err)
+
+		var ae *apierror.APIError
+		if errors.As(err, &ae) {
+			log.Printf("ae.Reason(): %v\n", ae.Reason())
+			log.Printf("ae.Details().Help.GetLinks(): %v\n", ae.Details().Help.GetLinks())
+		}
+
+		if s, ok := status.FromError(err); ok {
+			log.Printf("s.Message: %v\n", s.Message())
+			for _, d := range s.Proto().Details {
+				log.Printf("- Details: %v\n", d)
+			}
+		}
+		os.Exit(1)
+	}
+	// log.Printf("resp: %v", resp)
+
+	// Display the response.
+	for _, part := range resp.Candidates[0].Content.Parts {
+		fmt.Println("---")
+		fmt.Printf("%v\n", part)
+	}
+}
---
+++ prompt.md
@@ -0,0 +1,13 @@
+This is a text adventure game.
+
+At each turn, you will give a short description of the room or place where the user is, and will provide a short optional list of notable objects, people, or animals in the scene.
+
+Use a writing style that mimics a "choose your own adventure" novel. Each response starts with a descriptive chapter heading that denotes the current location. Use plain text without special formatting such as markdown.
+
+The user will respond with an action, like moving to a different location, inspecting an objection, interacting with another character, etc.
+
+You will keep track of items in the user's inventory. Users can pick up or drop items. Items can be used to make progress in the game. For example, a key might be used to open a lock, or a lantern might help light the way in a dark passageway.
+
+The game starts with the main character lying in bed and drifting away into a deep slumber. The next thing that happens is described as if the player has woken up, but in reality they're still in a dream, which means the world is quite strange and unlike the real waking world.
+
+Now, start the adventure by describing the initial starting scene and asking the player what they want to do next.
